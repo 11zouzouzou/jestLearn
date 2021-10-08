335aaf128a7516c00ba2fc8b14395c062eb02215
zouzouzou11 <1096174991@qq.com>
2021-10-08 10:24:19 +0800

    add three

--- helloWorld/sumTs.test.ts
+++ helloWorld/sumTs.test.ts
@@ -1,4 +1,12 @@
-import { sumTS } from "./sumTs";
+import { sumTS, THREETEST } from "./sumTs";
 test("adds 2 + 2 to equal 4", () => {
   expect(sumTS(2, 2)).toBe(4);
+});
+const threeTest = new THREETEST();
+test("camera far 1000", () => {
+  expect(threeTest.setCameraFar(1000)).toBe(1000);
+});
+
+test("camera far 0.5", () => {
+  expect(threeTest.setCameraNear(0.5)).toBe(0.5);
 });
--- helloWorld/sumTs.ts
+++ helloWorld/sumTs.ts
@@ -1,3 +1,24 @@
+import { PerspectiveCamera } from "three";
+
 export function sumTS(a: number, b: number) {
   return a + b;
 }
+
+class THREETEST {
+  camera: PerspectiveCamera;
+  constructor() {
+    this.camera = new PerspectiveCamera();
+  }
+
+  setCameraFar(v: number) {
+    this.camera.far = v;
+    return this.camera.far;
+  }
+
+  setCameraNear(v: number) {
+    this.camera.near = v;
+    return this.camera.near;
+  }
+}
+
+export { THREETEST };
